338c1492f38b29a4290fd7744611b10bee32c4ae
Harry Keefe <harry@harrykeefe.com>
2020-11-30 17:26:35 -0500

    Added contest display to user dashboard, created contest form, action and view

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,2 +1,27 @@
 class ContestsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @contest = Contest.new
+  end
+
+  def create
+    @contest = current_user.contests.build(contest_params)
+
+    respond_to do |format|
+      if @contest.save
+        format.html { redirect_to root_path, notice: "Contest successfully created" }
+        format.json { }
+      else
+        format.html { render :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def contest_params
+    params.require(:contest).permit(:name)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @user = current_user
+    @contests = current_user.contests
   end
 
 end
